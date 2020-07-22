dce132ef91fa708f9ab8804b224158fd998c3bd4
VAnhTrinh4869 <trinhvanh4869@gmail.com>
2020-07-22 21:03:15 +0700

    fix port

--- server.js
+++ server.js
@@ -6,16 +6,17 @@ var session = require('express-session');
 var mysql = require('mysql');
 const { query } = require('express');
 const { TIMEOUT } = require('dns');
+const PORT = process.env.PORT || 3000;
 var connection = mysql.createConnection({
-	host     : 'us-cdbr-east-02.cleardb.com',
-	user     : 'bf8a1b55c1e2e6',
-	password : '8c8d6ed8',
-	database : 'heroku_0d15c6611609f1f'
+	host: 'us-cdbr-east-02.cleardb.com',
+	user: 'bf8a1b55c1e2e6',
+	password: '8c8d6ed8',
+	database: 'heroku_0d15c6611609f1f'
 });
 //check connection
 connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected!');
+	if (err) throw err;
+	console.log('Connected!');
 });
 
 app.use(session({
@@ -26,46 +27,46 @@ app.use(session({
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+	extended: true
 }));
 // get login
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/web/login.html'));
+	res.sendFile(path.join(__dirname + '/web/login.html'));
 })
 // get material
 app.get('/server/web/css/style.css', function (req, res) {
-  res.sendFile('web/css/style.css', {root: __dirname })
+	res.sendFile('web/css/style.css', { root: __dirname })
 })
 
 app.get('/server/web/css/font-awesome.css', function (req, res) {
-  res.sendFile('web/css/font-awesome.css', {root: __dirname })
+	res.sendFile('web/css/font-awesome.css', { root: __dirname })
 })
 
 app.get('/server/web/images/1.jpg', function (req, res) {
-  res.sendFile('web/images/1.jpg', {root: __dirname })
+	res.sendFile('web/images/1.jpg', { root: __dirname })
 })
 app.get('/server/web/fonts/fontawesome-webfont.woff', function (req, res) {
-  res.sendFile('web/fonts/fontawesome-webfont.woff', {root: __dirname })
+	res.sendFile('web/fonts/fontawesome-webfont.woff', { root: __dirname })
 })
 
 app.get('/server/web/fonts/fontawesome-webfont.ttf', function (req, res) {
-  res.sendFile('web/fonts/fontawesome-webfont.ttf', {root: __dirname })
+	res.sendFile('web/fonts/fontawesome-webfont.ttf', { root: __dirname })
 })
 // get create account
 app.get('/create', function (req, res) {
 	res.sendFile(path.join(__dirname + '/web/createAccount.html'));
-  })
-app.get('/server/web/createAccount.html', function(req, res){
-	res.render('web/createAccount.html', {root: __dirname });
-  })
+})
+app.get('/server/web/createAccount.html', function (req, res) {
+	res.render('web/createAccount.html', { root: __dirname });
+})
 // get change password
 app.get('/change', function (req, res) {
 	res.sendFile(path.join(__dirname + '/web/changePassword.html'));
-  })
-app.get('/server/web/changePassword.html', function(req, res){
-	res.render('web/changePassword.html', {root: __dirname });
-  })
-app.get('web/account.html', function(request, response) {
+})
+app.get('/server/web/changePassword.html', function (req, res) {
+	res.render('web/changePassword.html', { root: __dirname });
+})
+app.get('web/account.html', function (request, response) {
 	if (request.session.loggedin) {
 		response.send('Welcome back, ' + request.session.username + '!');
 	} else {
@@ -74,11 +75,11 @@ app.get('web/account.html', function(request, response) {
 	response.end();
 });
 // Login
-app.post('/auth', function(request, response) {
+app.post('/auth', function (request, response) {
 	var username = request.body.username;
 	var password = request.body.password;
 	if (username && password) {
-		connection.query('SELECT * FROM account WHERE accountName = ? AND password = ?', [username, password], function(error, results) {
+		connection.query('SELECT * FROM account WHERE accountName = ? AND password = ?', [username, password], function (error, results) {
 			if (results.length > 0) {
 				console.log(results[0].accountName);
 				request.session.loggedin = true;
@@ -95,20 +96,19 @@ app.post('/auth', function(request, response) {
 	}
 });
 // Logout
-app.get('/logout',function(req,res){
-    req.session.destroy(function(err){
-        if(err){
-            console.log(err);
-        }
-        else
-        {
-        	res.redirect('/');
-        }
-    });
+app.get('/logout', function (req, res) {
+	req.session.destroy(function (err) {
+		if (err) {
+			console.log(err);
+		}
+		else {
+			res.redirect('/');
+		}
+	});
 
 });
 // create an account for admin
-app.post('/create', function(request, response) {
+app.post('/create', function (request, response) {
 	var userName = request.body.youremail;
 	var passWord = request.body.passwordNew;
 	const role = 1;
@@ -119,11 +119,11 @@ app.post('/create', function(request, response) {
 		role: role,
 		acc_status: acc_status
 	}
-	connection.query('INSERT INTO account SET ?', account, function(error){
-		if(error){
+	connection.query('INSERT INTO account SET ?', account, function (error) {
+		if (error) {
 			console.log(error.message);
 			response.end();
-		} else{
+		} else {
 			console.log('Create successfully!!!')
 			response.redirect('/');
 		}
@@ -132,15 +132,15 @@ app.post('/create', function(request, response) {
 });
 
 // Change password for admin
-app.post('/change', function(request, response) {
+app.post('/change', function (request, response) {
 	var accountName = request.body.userNameChange;
 	var password = request.body.newPassword;
 	let data = [password, accountName];
-	connection.query('UPDATE account SET password = ? WHERE accountName = ?', data, function(error, results, fields){
-		if(error){
+	connection.query('UPDATE account SET password = ? WHERE accountName = ?', data, function (error, results, fields) {
+		if (error) {
 			console.log(error.message);
 			response.end();
-		} else{
+		} else {
 			console.log('Rows affected: ', results.affectedRows);
 			response.redirect('/');
 		}
@@ -149,20 +149,20 @@ app.post('/change', function(request, response) {
 });
 
 // get data from mySql to fetch to account page
-app.get('/account', function(req, res, next){
-	connection.query('SELECT * FROM user',function(error, data, fields){
-		if(error){
+app.get('/account', function (req, res, next) {
+	connection.query('SELECT * FROM user', function (error, data, fields) {
+		if (error) {
 			console.log(error.message);
 		}
 		console.log(data.length);
-		res.render(__dirname + "/web/account.ejs",{userData: data})
+		res.render(__dirname + "/web/account.ejs", { userData: data })
 	});
 });
-var server = app.listen(3000, function () {
+var server = app.listen(PORT, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+	var host = server.address().address
+	var port = server.address().port
 
-  console.log("Node server running: http://%s:%s", host, port)
+	console.log("Node server running: http://%s:%s", host, port)
 
 })
